9db25aeb64db8b5700dee69c452834b63f773c18
Kinjal Patel <kinjalpatel.riki54@gmail.com>
2025-04-17 12:10:13 +0530

    For loop

---
+++ 5_for/for.go
@@ -0,0 +1,21 @@
+package main
+
+func main() {
+	// This is a simple while loop in Go
+	i := 0
+	for i < 10 {
+		println(i)
+		i++
+	}
+
+	// This is a simple for loop in Go
+	for i := 0; i < 10; i++ {
+		println(i)
+	}
+
+	// This is a simple for loop in Go
+	// with a range clause in Go
+	for i := range 10 {
+		println(i)
+	}
+}
